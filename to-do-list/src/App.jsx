a584ed6b9d243f8886044fe8ee4f8912583a3469
alikzilla <nuspekovalihan@gmail.com>
2024-04-03 22:02:10 +0500

    added reducer for manage tasks

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,48 +1,51 @@
-import React, { useState } from 'react';
-import Header from './components/Header/Header';
-import Button from './components/Button/Button';
-import Input from './components/Input/Input';
-import TaskBlock from './components/TaskBlock/TaskBlock';
-import './App.css';
+  import React, { useState } from 'react';
+  import { useDispatch, useSelector } from 'react-redux';
+  import { addTask, removeTask } from './main';
+  import Header from './components/Header/Header';
+  import Button from './components/Button/Button';
+  import Input from './components/Input/Input';
+  import TaskBlock from './components/TaskBlock/TaskBlock';
+  import './App.css';
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-  const [taskText, setTaskText] = useState("");
+  function App() {
+    const dispatch = useDispatch();
 
-  const handleAddTask = () => {
-    if (taskText.trim() !== "") {
-      setTasks([...tasks, taskText]);
-      setTaskText("");
-    }
-    console.log('clicked')
-  };
+    const tasks = useSelector(state => state.tasks);
 
-  const handleRemoveTask = (index) => {
-    const updatedTasks = [...tasks]
-    updatedTasks.splice(index, 1)
-    setTasks(updatedTasks)
+    const [taskText, setTaskText] = useState('');
+
+    const handleInputChange = (event) => {
+      setTaskText(event.target.value);
+    };
+
+    const handleAddTask = () => {
+      if (taskText.trim() !== '') {
+        dispatch(addTask(taskText));
+        setTaskText('');
+      }
+      console.log(tasks)
+    };
+
+    const handleRemoveTask = (index) => {
+      dispatch(removeTask(index))
+    };
+
+    return (
+      <div className='app'>
+        <Header />
+        <section className='toDoBlock'>
+          <div className='control'>
+            <Input type="text" placeholder="Enter Task for List" value={taskText} onChange={handleInputChange} />
+            <Button label="Enter" onClick={handleAddTask} />
+          </div>
+          <ul>
+            {tasks.map((task, index) => (
+              <TaskBlock key={index} text={task} onClick={() => handleRemoveTask(index)} />
+            ))}
+          </ul>
+        </section>
+      </div>
+    );
   }
 
-  const handleInputChange = (event) => {
-    setTaskText(event.target.value);
-  };
-
-  return (
-    <div className='app'>
-      <Header />
-      <section className='toDoBlock'>
-        <div className='control'>
-          <Input type="text" placeholder="Enter Task for List" value={taskText} onChange={handleInputChange} />
-          <Button label="Enter" onClick={handleAddTask} />
-        </div>
-        <ul>
-          {tasks.map((task, index) => (
-            <TaskBlock key={index} text={task} onClick={() => handleRemoveTask(index)} />
-          ))}
-        </ul>
-      </section>
-    </div>
-  );
-}
-
-export default App;
+  export default App;
--- to-do-list/src/main.jsx
+++ to-do-list/src/main.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+const defaultState = {
+  tasks: []
+}
+
+export const addTask = (taskText) => ({
+  type: "ADD_TASK",
+  payload: taskText,
+});
+
+export const removeTask = (index) => ({
+  type: "REMOVE_TASK",
+  payload: index,
+})
+ 
+const reducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case "ADD_TASK":
+      return {tasks: [...state.tasks, action.payload]};
+    case "REMOVE_TASK":
+      return {tasks: state.tasks.filter((_, index) => index !== action.payload)};
+    default:
+      return state;
+  } 
+}
+
+const store = createStore(reducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
